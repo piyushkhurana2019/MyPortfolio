87ac612ff36379474b1e395955e11e902be1653c
piyushkhurana2019 <piyushkhurana159@gmail.com>
2023-11-22 01:25:07 +0530

    CSS Fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,10 @@ const Navbar = () => {
   return (
 
       <header className="header">
-        <NavLink to='/' className='w-10 -10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md'>
+        <NavLink to='/' className='w-10  rounded-lg bg-white items-center justify-center flex font-bold shadow-md fixed left-10 top-5'>
             <p className="blue-gradient_text">PK</p>
         </NavLink>
-            <nav className="flex text-lg gap-7 font-medium">
+            <nav className="flex text-lg gap-7 font-medium fixed right-14 top-5">
                 <NavLink to='about' className={({ isActive })=> isActive ? 'text-blue-500' : 'text-black'}>
                     About
                 </NavLink>
