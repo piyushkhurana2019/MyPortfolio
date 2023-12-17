98075b9d7d09a49702cc8394b07def32f6b22aa2
piyushkhurana2019 <piyushkhurana159@gmail.com>
2023-12-18 02:31:01 +0530

    Download resume

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,29 @@
 import { NavLink } from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { Color } from "three";
+import '/src/index.css'
 const Navbar = () => {
+    const handleDownload = () => {
+        // Add the logic to trigger the download here
+        // For example, you can use window.open or create a download link
+        const resumeUrl = 'https://drive.google.com/file/d/1gOXtzPa5z7UHyjbdHNPzcidcUpavtRtt/view?usp=sharing';
+
+        // Open the resume URL in a new window to trigger the download
+        window.open(resumeUrl, '_blank');
+      };
+    
   return (
 
       <header className="header">
         <NavLink to='/' className='w-10  rounded-lg bg-white items-center justify-center flex font-bold shadow-md fixed left-10 top-5'>
-            <p className="blue-gradient_text">PK</p>
+        <div>
+      {/* Button with download icon and same color */}
+      <button className="downloader" onClick={handleDownload}>
+        <FontAwesomeIcon icon={faDownload} color="#0072ff" />
+        <p className="tooltip-text">Download Resume</p>
+      </button>
+    </div>
         </NavLink>
             <nav className="flex text-lg gap-7 font-medium fixed right-14 top-5">
                 <NavLink to='about' className={({ isActive })=> isActive ? 'text-blue-500' : 'text-black'}>
